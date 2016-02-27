7667dc4f83e5da5ac57c203e1aab19954bab940f
bdogan <bdogan85@gmail.com>
2016-02-27 18:05:12 +0200

    Bug Fixes

--- nginx-php70/build/platin_init.d/06_php_fpm.sh
+++ nginx-php70/build/platin_init.d/06_php_fpm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Check if sock not created
-if [ ! -f /run/php/php7.0-fpm.sock ]; then
+if [ ! -e /run/php/php7.0-fpm.sock ]; then
     mkdir /run/php/
     touch /run/php/php7.0-fpm.sock
     chmod 777 /run/php/php7.0-fpm.sock
@@ -20,15 +20,20 @@ do
     echo "Adding php-conf files from $BASEFOLDER/$tag/php-conf to /etc/php/7.0/fpm/conf.d/"
     cp -rf $BASEFOLDER/$tag/php-conf/* /etc/php/7.0/fpm/conf.d/
   fi
-
-  if [ -e $BASEFOLDER/$APPNAME/php-conf ]; then
-    echo "Adding php-conf files from $BASEFOLDER/$APPNAME/php-conf to /etc/php/7.0/fpm/conf.d/"
-    cp -rf $BASEFOLDER/$APPNAME/php-conf/* /etc/php/7.0/fpm/conf.d/
-  fi
 done
+
+if [ -e $BASEFOLDER/$APPNAME/php-conf ]; then
+  echo "Adding php-conf files from $BASEFOLDER/$APPNAME/php-conf to /etc/php/7.0/fpm/conf.d/"
+  cp -rf $BASEFOLDER/$APPNAME/php-conf/* /etc/php/7.0/fpm/conf.d/
+fi
+
+if [ -f $BASEFOLDER/$APPNAME/www.conf ]; then
+  echo "Adding www.conf file from $BASEFOLDER/$APPNAME/www.conf to /etc/php/7.0/fpm/pool.d/www.conf"
+  cp -f $BASEFOLDER/$APPNAME/www.conf /etc/php/7.0/fpm/pool.d/www.conf
+fi
 
 # Restart php5 service, if it exists.
 if [ -e /etc/service/php/run ]; then
-  echo "Reloading php7.0-fpm."
-  killall php7.0-fpm
+  echo "Reloading php-fpm7.0."
+  killall php-fpm7.0
 fi
